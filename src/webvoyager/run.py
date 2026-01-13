d9da75121236e6a5705661ae7c812976e5f9c43c
hy <yuanfeng@tensoropera.com>
2026-01-13 23:54:39 +0800

    use webdriver manager

--- src/webvoyager/run.py
+++ src/webvoyager/run.py
@@ -14,7 +14,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
 from dotenv import load_dotenv
+
+# Try to use webdriver-manager if available
+try:
+    from webdriver_manager.chrome import ChromeDriverManager
+    USE_WEBDRIVER_MANAGER = True
+except ImportError:
+    USE_WEBDRIVER_MANAGER = False
 load_dotenv()
 
 from .webvoyager_prompts import SYSTEM_PROMPT, SYSTEM_PROMPT_TEXT_ONLY
